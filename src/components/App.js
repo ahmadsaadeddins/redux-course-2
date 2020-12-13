f28485f288f78defc133d20fd561cff9c13050a5
Ahmad Saadeddin <AS@ahm4d.com>
2020-12-13 21:40:25 +0200

    authed user

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,13 @@
-import * as React from 'react'
+import * as React from "react";
+import { useSelector } from "react-redux";
 
 export default function App() {
+  const store = useSelector((store) => store);
+  console.log("store", store);
   return (
     <div>
       Redux Polls
+      <p>Add</p>
     </div>
-  )
+  );
 }
